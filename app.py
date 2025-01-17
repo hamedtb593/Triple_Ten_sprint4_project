dbd22d044d9621b258600bddfe2ca360b1637510
hamedtb593 <hamedtaeb@yahoo.com>
2025-01-16 21:19:41 -0800

    Fixing CSV resource file

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pyarrow as pa   # Looks like I needed this due to Render error that I got
 st.write("Using PyArrow version:", pa.__version__)
 
 # loading the dataset 
-df = pd.read_csv('https://https://triple10-sprint4-project.onrender.com//vehicles_us.csv')
+df = pd.read_csv('https://triple10-sprint4-project.onrender.com//vehicles_us.csv')
 # Extracting the manufacturer from the model column
 df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
 # drop rows with missing values on the model_year column
